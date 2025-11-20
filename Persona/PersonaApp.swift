529cfd326b60deeaf8fa921409f6f913a9739183
Dennis Lam <hinyeung599@outlook.com>
2025-11-19 16:33:07 -0800

    Initial Commit

---
+++ Persona/PersonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonaApp.swift
+//  Persona
+//
+//  Created by Dennis Lam on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
